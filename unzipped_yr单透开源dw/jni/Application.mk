569008a649548d22b5bc73f56691cec1968b51d1
zhuimingyiling <a2493708787z@gmail.com>
2026-01-18 18:27:28 +0800

    Delete unzipped_yr单透开源dw/jni directory

--- unzipped_yr单透开源dw/jni/Application.mk
+++
@@ -1,9 +0,0 @@
-APP_ABI := arm64-v8a
-APP_PLATFORM := android-21
-APP_STL := c++_static
-#APP_STL := c++_shared
-APP_OPTIM := release
-
-
-
-
--- unzipped_yr单透开源dw/jni/src/Android_my_imgui/my_imgui.cpp
+++
@@ -1,3 +0,0 @@
-#include <unistd.h>
-#include "my_imgui.h"
-#include "imgui_internal.h"
